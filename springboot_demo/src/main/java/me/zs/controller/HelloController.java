da56938a456892eebe3ddb60dfe9f6a11112dff0
zhangshi <zhangshi@me.zs>
2019-09-09 14:49:10 +0800

    增加了根据id查询功能

--- springboot_demo/src/main/java/me/zs/controller/HelloController.java
+++ springboot_demo/src/main/java/me/zs/controller/HelloController.java
@@ -1,27 +1,30 @@
 package me.zs.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import me.zs.pojo.User;
+import me.zs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
 
-@Controller
+@RestController
 //使用该注解相当于@Controller和@ResponseBody的结合，此时的Controller无法返回jsp或者html页面
 //@RestController
 @Slf4j
+@RequestMapping("/user")
 public class HelloController {
 
     /*@Autowired
     private DataSource dataSource;*/
 
-    @RequestMapping("/hello")
-    @ResponseBody
-    public String hello(){
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("{id}")
+    public User hello(@PathVariable("id") Long id){
         log.debug("hello method is running");
-        return "hello springboot!!!";
+        return userService.queryById(id);
     }
 }
--- springboot_demo/src/test/java/me/zs/mapper/UserMapperTest.java
+++ springboot_demo/src/test/java/me/zs/mapper/UserMapperTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -20,5 +22,24 @@ public class UserMapperTest {
     public void testQuery(){
         User user = userMapper.selectByPrimaryKey(8L);
         System.out.println(user);
+    }
+
+    @Test
+    public void testInsert(){
+        User user = new User();
+        user.setUserName("zhangsan");
+        user.setPassword("123456");
+        user.setName("张三");
+        user.setAge(27);
+        user.setSex(1);
+        user.setBirthday(new Date(92,6,28,16,50,39));
+        user.setCreated(new Date());
+        user.setUpdated(new Date());
+        userMapper.insert(user);
+    }
+
+    @Test
+    public void testDate(){
+        System.out.println(new Date(92,6,28,16,50,39));
     }
 }
