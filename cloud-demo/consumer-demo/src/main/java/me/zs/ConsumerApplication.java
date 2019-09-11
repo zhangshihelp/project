e7b34aae3b5378199f5f96354ad11ab48175887f
zhangshi <zhangshi@me.zs>
2019-09-11 08:19:35 +0800

    两个eureka的相互注册，将两个服务都注册到这两个注册中心，保证了高可用。 并使用DiscoveryClient类进行服务发现，完成调用。

--- cloud-demo/consumer-demo/src/main/java/me/zs/ConsumerApplication.java
+++ cloud-demo/consumer-demo/src/main/java/me/zs/ConsumerApplication.java
@@ -2,10 +2,12 @@ package me.zs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- cloud-demo/consumer-demo/src/main/java/me/zs/consumer/web/ConsumerController.java
+++ cloud-demo/consumer-demo/src/main/java/me/zs/consumer/web/ConsumerController.java
@@ -2,11 +2,15 @@ package me.zs.consumer.web;
 
 import me.zs.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/consumer")
@@ -15,9 +19,16 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id) {
-        String url = "http://localhost:8000/user/" + id;
+        // 根据服务id获取实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        // 从实例中取出ip和端口
+        ServiceInstance instance = instances.get(0);
+        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id;
         return restTemplate.getForObject(url, User.class);
     }
 }
