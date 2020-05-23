85b1ec386794466af8d5ebc02011d582cc601b8a
zhangshi <zhangshi@me.zs>
2020-05-23 17:10:54 +0800

    1

--- cloud-demo/user-service/src/main/java/me/zs/user/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package me.zs.user.mapper;
-
-import me.zs.user.pojo.User;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserMapper extends Mapper<User> {
-}
--- springboot_demo/src/main/java/me/zs/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package me.zs.mapper;
-
-import me.zs.pojo.User;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserMapper extends Mapper<User> {
-}
